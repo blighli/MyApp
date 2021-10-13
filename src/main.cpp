d987f4a8906de5805d3f6e036dc9f47b28147573
liqilei <blighli@gmail.com>
2021-10-14 06:21:15 +0800

    fix GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 #include <FreeImage/FreeImage.h>
 
 int main() {
+    glfwInit();
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
